aa034d0bf0464c72dd8eeade4584641c39de7a8c
Karl Rink <karl@rink.us>
2021-12-20 20:37:52 -0800

    20211220-0

--- src/logstream/__main__.py
+++ src/logstream/__main__.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 
-"""dir.__main__: executed when jumpcloud directory is called as script."""
+"""dir.__main__: executed when directory is called as script."""
 
 from .logstream import main
 main()
