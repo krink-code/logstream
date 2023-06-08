e3063e8f6cd8591ad78d28999f8797adeff09ab8
Karl Rink <karl_rink@comcast.com>
2023-06-08 10:41:14 -0700

    🌈 2023-06-08 1.0.0

--- golang/logstream/logstream.go
+++ golang/logstream/logstream.go
@@ -5,7 +5,10 @@ import (
     "os"
     "os/exec"
     "runtime"
+    "bufio"
+    "regexp"
 )
+
 
 func Stream() error {
 
@@ -27,3 +30,56 @@ func Stream() error {
 
     return nil
 }
+
+
+func Grep(word string) error {
+
+        var cmd *exec.Cmd
+
+        switch runtime.GOOS {
+        case "windows":
+                fmt.Println("TODO: Windows")
+                return nil
+        case "darwin":
+                cmd = exec.Command("log", "stream", "--style", "ndjson")
+        case "linux":
+                cmd = exec.Command("journalctl", "-f", "-o", "json")
+        default:
+                return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
+        }
+
+        stdoutPipe, err := cmd.StdoutPipe()
+        if err != nil {
+                return err
+        }
+
+        err = cmd.Start()
+        if err != nil {
+                return err
+        }
+
+        go func() {
+                defer cmd.Wait()
+                scanner := bufio.NewScanner(stdoutPipe)
+                for scanner.Scan() {
+                        line := scanner.Text()
+                        if grep(word, line) {
+                                fmt.Println(line)
+                        }
+                }
+        }()
+
+        err = cmd.Wait()
+        if err != nil {
+                return err
+        }
+
+        return nil
+}
+
+func grep(pattern string, text string) bool {
+        regex := regexp.MustCompile(pattern)
+        return regex.MatchString(text)
+}
+
+
--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"os"
 
-    "gitlab.com/krink/logstream/golang/logstream"
+	"gitlab.com/krink/logstream/golang/logstream"
 )
 
+var version = "1.0.0"
 
 func main() {
 
-    err := logstream.Stream()
-
-    if err != nil {
-        panic(err)
-    }
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+		switch arg {
+		case "grep":
+			if len(os.Args) > 2 {
+				pattern := os.Args[2]
+				err := logstream.Grep(pattern)
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				fmt.Println("Pattern missing. Usage: program grep <pattern>")
+			}
+		case "--help":
+			printUsage()
+		case "--version":
+			fmt.Println("Version ", version)
+		default:
+			fmt.Println("Invalid command. Usage: program grep <pattern>")
+		}
+	} else {
+		err := logstream.Stream()
+		if err != nil {
+			panic(err)
+		}
+	}
 
 }
+
+func printUsage() {
+	usage := `Usage: program [command] [options]
+
+Commands:
+  grep    Filter log output based on a pattern
+
+Options:
+  --help  Display this help message
+  --version  Display version
+  `
+
+	fmt.Println(usage)
+}
+
